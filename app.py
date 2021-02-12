144d1f2c21f32efea1c007c86107d040feefdf86
artmexbet <60144997+artmexbet@users.noreply.github.com>
2021-02-12 23:11:37 +0700

    г

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///tables.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///table.db'
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(300), nullable=False, primary_key=True)
+    password = db.Column(db.String(300), nullable=False)
     name = db.Column(db.String(300), nullable=False)
-    class_digit = db.Column(db.String(4), nullable=False)
-    stage = db.Column(db.Integer, nullable=False)
+    class_digit = db.Column(db.Integer, nullable=False)
+    stage = db.Column(db.String(4), nullable=False)
     is_command = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
@@ -20,6 +22,7 @@ class User(db.Model):
 class UserResult(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     maths = db.Column(db.Integer, nullable=False, default=0)
+
     # Дальше живут драконы
 
     def __repr__(self):
@@ -29,9 +32,18 @@ class UserResult(db.Model):
 @app.route("/add_user", methods=["POST", "GET"])
 def add_user():
     if request.method == "POST":
-        pass
+        data = request.form
+        if data["Password"] != data["Password_confirmation"]:
+            return render_template("reg.html", is_password_possible=False)
+        user = User(name=f'{data["Name"]} {data["Surname"]} {data["Lastname"]}', class_digit=int(data["Stage_number"]),
+                    stage=data["Stage_letter"], is_command=(1 if data["Teamed"] == "on" else 0), login=data['Login'],
+                    password=data['Password'])
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect("/")
     else:
-        return render_template("main.html")
+        return render_template("reg.html")
 
 
 @app.route("/")
