8424045c0dc9d1edcf2bfbd8d32dd4c5aeec8a55
artmexbet <maydurov2004@yandex.ru>
2021-03-01 23:19:57 +0700

    some changes

---
+++ main_2.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///table.db'
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def main():
+    return "Это backend часть этого сайта"
+
+
+@app.route("/users", methods=["GET", "POST"])
+def users():
+    if request.method == "GET":
+        return {"id": ["params"]}
+    else:
+        # Тут мы будем добавлять в базу данных людей, если их там нет
+        pass
+
+
+@app.route("/add_result/<subject>/<int:user_id>", methods=["POST"])
+def add_result(subject: str, user_id: int):
+    """
+    Здесь добавляем результаты людям
+    :param subject: Имя предмета, по которому мы будем добавлять результаты
+    :param user_id: id пользователя, которому будут добавлены баллы
+    :return: Прога вернёт вердикт, в нормальном положении это что-то вроде "ok"
+    """
+    pass
+
+
+if __name__ == '__main__':
+    app.run()
